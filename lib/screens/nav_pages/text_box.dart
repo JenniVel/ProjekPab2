45178cdc13ae01d8b25a5c457741669e5385e850
intaansintan <intansanu_2226250109@mhs.mdp.ac.id>
2024-05-24 17:02:05 +0700

    updateeee

--- lib/screens/nav_pages/text_box.dart
+++ lib/screens/nav_pages/text_box.dart
@@ -26,23 +26,59 @@ class MyTextBox extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              //Section name
+              // Section name
               Text(
                 sectionName,
                 style: TextStyle(color: Colors.yellowAccent),
               ),
-              //edit button
+              // Edit button
               IconButton(
                 onPressed: onPressed,
                 icon: Icon(
-                  Icons.settings,
-                  color: Colors.grey,
+                  Icons.edit,
+                  color: Color(0xFF176FF2),
                 ),
               ),
             ],
           ),
 
-          //text
+          // Text
+          Text(text),
+        ],
+      ),
+    );
+  }
+}
+
+class TextBiasa extends StatelessWidget {
+  final String text;
+  final String sectionName;
+
+  const TextBiasa({
+    super.key,
+    required this.text,
+    required this.sectionName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.cyan,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      padding: const EdgeInsets.only(left: 15, bottom: 15),
+      margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Section name
+          Text(
+            sectionName,
+            style: TextStyle(color: Colors.yellowAccent),
+          ),
+          const SizedBox(height: 5),
+          // Text
           Text(text),
         ],
       ),
