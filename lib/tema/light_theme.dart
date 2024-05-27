0f1fb28958ebeb0cf7fa6525484242197a00a490
intaansintan <intansanu_2226250109@mhs.mdp.ac.id>
2024-05-27 18:46:31 +0700

    update tema terbaru

--- lib/tema/light_theme.dart
+++ lib/tema/light_theme.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
 final ThemeData lightTheme = ThemeData(
-  primaryColor: Color(0xFF176FF2),
+  primaryColor: Color(0xFF004B90),
   brightness: Brightness.light,
-  scaffoldBackgroundColor: Colors.white,
+  scaffoldBackgroundColor: Color(0xFFF1F8FF),
   appBarTheme: AppBarTheme(
-    backgroundColor: Color(0xFF176FF2),
+    backgroundColor: Color.fromARGB(255, 158, 199, 246),
     iconTheme: IconThemeData(color: Colors.white),
   ),
   floatingActionButtonTheme: FloatingActionButtonThemeData(
     backgroundColor: Color(0xFF176FF2),
+  ),
+  textTheme: TextTheme(
+    bodyText1: TextStyle(color: Color(0xFF176FF2)),
   ),
 );
