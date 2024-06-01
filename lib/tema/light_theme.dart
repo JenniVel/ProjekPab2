55af2cce43fabb47d3d54b61041b6a0d609a7a36
intaansintan <intansanu_2226250109@mhs.mdp.ac.id>
2024-06-01 22:35:11 +0700

    teslokasiii

--- lib/tema/light_theme.dart
+++ lib/tema/light_theme.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
 final ThemeData lightTheme = ThemeData(
-    primaryColor: Color(0xFF004B90),
+    primaryColor: Color(0xFF004B90), //text judul
     brightness: Brightness.light,
-    scaffoldBackgroundColor: Color.fromARGB(255, 228, 239, 250),
+    scaffoldBackgroundColor: Color.fromARGB(255, 228, 239, 250), //background
     appBarTheme: AppBarTheme(
-      backgroundColor: Color.fromARGB(255, 55, 119, 192),
-      iconTheme: IconThemeData(color: Colors.white),
+      backgroundColor: Color.fromARGB(255, 55, 119, 192), //lupaaa
+      iconTheme: IconThemeData(color: Colors.white), // icon
     ),
     floatingActionButtonTheme: FloatingActionButtonThemeData(
         backgroundColor: Color.fromARGB(255, 227, 231, 242)),
     textTheme: TextTheme(
       bodyText1: TextStyle(color: Color.fromARGB(255, 93, 152, 218)),
     ),
-    backgroundColor: Color.fromARGB(255, 211, 221, 232));
+    backgroundColor: Color.fromARGB(255, 211, 221, 232)); //nav bawah, label
